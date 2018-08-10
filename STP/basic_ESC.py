e7bdffa434977895f39eee09b9985ad7419caa3d
L-Schade <lischade@uni-bremen.de>
2018-08-10 23:32:06 +0200

    basic_ESC update

--- STP/basic_ESC.py
+++ STP/basic_ESC.py
@@ -1,54 +1,56 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)    # GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
-t1 = GPIO.PWM(7, 50)
+m1 = GPIO.PWM(17, 50)
 
-t1.start(0)
+m1.start(10)
+print("gestartet")
 
-t1.ChangeDutyCycle(7.5)
+m1.ChangeDutyCycle(100)
 time.sleep(3)
 
 try:
-while True:
-t1.ChangeDutyCycle(9.5)
-time.sleep(3)
-t1.ChangeDutyCycle(5.5)
-time.sleep(3)
-t1.ChangeDutyCycle(7.5)
-time.sleep(3)
+    while True:
+        m1.ChangeDutyCycle(9.5)
+        time.sleep(3)
+        m1.ChangeDutyCycle(1.5)
+        time.sleep(3)
+        m1.ChangeDutyCycle(7.5)
+        time.sleep(3)
 
 except KeyboardInterrupt:
-print(“User Cancelled”)
+    print("User Cancelled")
 
 finally:
-t1.stop()
-GPIO.cleanup()
-quit()
+    m1.stop()
+    GPIO.cleanup()
+    quit()
 
 
 
-GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
-  
-GPIO.setup(25, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
-  
-p = GPIO.PWM(25, 50)    # create an object p for PWM on port 25 at 50 Hertz  
-                        # you can have more than one of these, but they need  
-                        # different names for each port   
-                        # e.g. p1, p2, motor, servo1 etc.  
-  
-p.start(50)             # start the PWM on 50 percent duty cycle  
-                        # duty cycle value can be 0.0 to 100.0%, floats are OK  
-  
-p.ChangeDutyCycle(90)   # change the duty cycle to 90%  
-  
-p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
-                        # e.g. 100.5, 5.2  
-  
-p.stop()                # stop the PWM output  
-  
-GPIO.cleanup()          # when your program exits, tidy up after yourself  
+# GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
+#
+# GPIO.setup(4, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
+#
+# p = GPIO.PWM(4, 1000)    # create an object p for PWM on port 25 at 50 Hertz
+#                         # you can have more than one of these, but they need
+#                         # different names for each port
+#                         # e.g. p1, p2, motor, servo1 etc.
+#
+# p.start(50)             # start the PWM on 50 percent duty cycle
+#                         # duty cycle value can be 0.0 to 100.0%, floats are OK
+#
+# time.sleep(10)
+# p.ChangeDutyCycle(90)   # change the duty cycle to 90%
+#
+# p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)
+#                         # e.g. 100.5, 5.2
+#
+# p.stop()                # stop the PWM output
+#
+# GPIO.cleanup()          # when your program exits, tidy up after yourself
