9b9792c30401480c99d66194d622e07360020857
L-Schade <lischade@uni-bremen.de>
2018-09-08 18:12:27 +0200

    update alles

--- STP/Motoren/motorTest.py
+++ STP/Motoren/motorTest.py
@@ -7,24 +7,27 @@ GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 1000) # GPIO 19 als PWM mit 50Hz
 p.start(5) # Initialisierung
+
 try:
 	while True:
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(5)
-		time.sleep(0.5)
+		p.ChangeDutyCycle(0  )
+		time.sleep(0.050)
+
+
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
+		# p.ChangeDutyCycle(5)
+		# time.sleep(0.5)
 except KeyboardInterrupt:
 	p.stop()
 	GPIO.cleanup()
