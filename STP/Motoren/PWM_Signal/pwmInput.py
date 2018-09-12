4e2acdf1e5b6dd801f991182a1a4896b01e7a330
L-Schade <lischade@uni-bremen.de>
2018-09-12 11:20:30 +0200

    PWM_Signal

---
+++ STP/Motoren/PWM_Signal/pwmInput.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+# import time
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+
+pin_in = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_in, GPIO.IN)
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+
+def animate(i):
+    graph_data = open('example.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.plot(xs, ys)
+
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
