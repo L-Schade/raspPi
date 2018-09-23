d8b1e105e2b75f4bd73a29a05e71acb32f5e8c22
L-Schade <lischade@uni-bremen.de>
2018-09-23 23:33:52 +0200

    update

--- STP/Motoren/Mikrocontroller/pwmTest.py
+++ STP/Motoren/Mikrocontroller/pwmTest.py
@@ -15,8 +15,8 @@ pi=pigpio.pi()
 pwm=wavePWM.PWM(pi)
 
 
-pwm.set_frequency(1000)    		# 1k Hz oder 10k Hz
-##pwm.set_cycle_time(micros)	# in microseconds
+##pwm.set_frequency(1000)                   # 1k Hz oder 10k Hz
+pwm.set_cycle_time(100)                                # in microseconds 4
 print(pwm.frequency)
 print(pwm.get_cycle_length())
 print(pwm.get_GPIO_settings(4))
@@ -34,25 +34,34 @@ print("\n")
 ##pwm.set_pulse_start_and_length_in_fraction(23,0.33,0.75)
 ##pwm.set_pulse_start_and_length_in_fraction(24,0.66,0.75)
 
-pwm.set_pulse_start_and_length_in_fraction(4,0.33,0.2)     ## 0.2
-pwm.set_pulse_start_and_length_in_fraction(23,0.66,0.2)
-pwm.set_pulse_start_and_length_in_fraction(24,0,0.2)
+##pwm.set_pulse_start_and_length_in_fraction(4,0.33,0.2)     ## 0.2
+##pwm.set_pulse_start_and_length_in_fraction(23,0.66,0.2)
+##pwm.set_pulse_start_and_length_in_fraction(24,0,0.2)
 
-pwm.update()
-time.sleep(2)
+##pwm.update()
+##time.sleep(2)
 ##pwm.cancel()
 
-pwm.set_pulse_start_and_length_in_fraction(4,0,0.2)     ## 0.2
-pwm.set_pulse_start_and_length_in_fraction(23,0.33,0.2)
-pwm.set_pulse_start_and_length_in_fraction(24,0.66,0.2)
+##pwm.set_pulse_start_and_length_in_fraction(4,0,0.2)     ## 0.2
+##pwm.set_pulse_start_and_length_in_fraction(23,0.33,0.2)
+##pwm.set_pulse_start_and_length_in_fraction(24,0.66,0.2)
 
 ##time.sleep(0.125)
 ##time.sleep(2)
 
 # haengt noch manchmal
-##pwm.set_pulse_start_and_length_in_micros(4,0,80)        # ca 4 Grad
-##pwm.set_pulse_start_and_length_in_micros(23,0.33,80)
-##pwm.set_pulse_start_and_length_in_micros(24,0.66,80)
+##pwm.set_pulse_start_and_length_in_micros(4,0,5)        # ca 4 Grad
+##pwm.set_pulse_start_and_length_in_micros(23,2.33,1)
+##pwm.set_pulse_start_and_length_in_micros(24,5.66,5)
+
+pwm.set_pulse_start_and_length_in_micros(4,10,80)        # ca 4 Grad
+pwm.set_pulse_start_and_length_in_micros(23,20,25)
+pwm.set_pulse_start_and_length_in_micros(24,30,20)
+
+# relativ gute Einstellung
+##pwm.set_pulse_start_and_length_in_micros(4,0,3.75)        # ca 3-4 Grad
+##pwm.set_pulse_start_and_length_in_micros(23,1.33,1.5)
+##pwm.set_pulse_start_and_length_in_micros(24,2.66,1.5)     # je groesser desto groesserer Wibkel
 
 
 pwm.update()
@@ -63,6 +72,7 @@ print(pwm.get_GPIO_settings(4))
 print(pwm.get_GPIO_settings(23))
 print(pwm.get_GPIO_settings(24))
 
+##time.sleep(2)
 pwm.cancel()
 
 ##pwm.set_pulse_length_in_micros(4, 500) 
